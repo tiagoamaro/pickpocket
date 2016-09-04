5879a17a1e21fc45f70fc69b2b9fcbbce6e2c3e9
Tiago Amaro <tiagopadrela@gmail.com>
2016-09-04 22:29:54 +0200

    add OAuth tests with VCR

--- spec/lib/oauth_spec.rb
+++ spec/lib/oauth_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
+require 'support/setup/vcr'
 
 module Pickpocket
-  RSpec.describe Oauth do
+  RSpec.describe Oauth, :vcr do
+    let(:tempfile) { Tempfile.new('tempfile_logger') }
+    let(:tempfile_logger) { ::Logger.new(tempfile) }
+    let(:oauth) { described_class.new }
+
+    before(:each) do
+      oauth.logger = tempfile_logger
+    end
+
+    describe '#request_authorization' do
+      it 'requests authorization from pocket, saving its token and printing given URL' do
+        oauth.request_authorization
+
+        token_handler = oauth.token_handler
+        tempfile.rewind
+
+        expect(tempfile.read).to include('https://getpocket.com/auth/authorize?request_token=e6e388d8-b604-70d6-1f8e-49c2aa&redirect_uri=https://getpocket.com')
+        expect(token_handler.read).to eq('e6e388d8-b604-70d6-1f8e-49c2aa')
+      end
+    end
+
+    describe '#authorize' do
+      it "authorizes Pickpocket using user's token" do
+        result = oauth.authorize
+        expect(result.code).to eq('200')
+      end
+    end
   end
 end

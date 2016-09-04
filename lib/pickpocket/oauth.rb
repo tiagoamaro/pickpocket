3936a75646a7995bfa2dfe7693897e8bcea002b5
Tiago Amaro <tiagopadrela@gmail.com>
2016-09-04 23:43:43 +0200

    parse query string with Ruby’s CGI

--- lib/pickpocket/oauth.rb
+++ lib/pickpocket/oauth.rb
@@ -1,5 +1,6 @@
-require 'uri'
+require 'cgi'
 require 'net/http'
+require 'uri'
 
 module Pickpocket
   class Oauth
@@ -18,8 +19,8 @@ module Pickpocket
           redirect_uri: POCKET_HOMEPAGE
       })
 
-      response_token = response.body.match(/code=(?<token>.*)/)[:token]
-      auth_url       = %Q{https://getpocket.com/auth/authorize?request_token=#{response_token}&redirect_uri=#{POCKET_HOMEPAGE}}
+      response_token = CGI::parse(response.body)['code'][0]
+      auth_url       = %Q{#{POCKET_USER_AUTHORIZE_URL}?request_token=#{response_token}&redirect_uri=#{POCKET_HOMEPAGE}}
 
       logger.info %Q{To continue, authorize this app opening the following URL: #{auth_url}}
       token_handler.save(response_token)
@@ -28,10 +29,14 @@ module Pickpocket
     def authorize
       response_token = token_handler.read
       uri            = URI(POCKET_OAUTH_AUTHORIZE_URL)
-      Net::HTTP.post_form(uri, {
+
+      response = Net::HTTP.post_form(uri, {
           consumer_key: CONSUMER_KEY,
           code:         response_token
       })
+
+      response_token = CGI::parse(response.body)['access_token'][0]
+      token_handler.save(response_token)
     end
   end
 end

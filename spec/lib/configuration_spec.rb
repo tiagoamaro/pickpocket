1ebe838333c2d22523d4e53ac4d32dba9859baeb
Tiago Amaro <tiagopadrela@gmail.com>
2016-09-06 20:28:11 +0200

    include article list file in gem config

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -8,8 +8,11 @@ module Pickpocket
 
     describe 'default configurations' do
       it { expect(config.home_folder).to eq(File.expand_path('~/.pickpocket')) }
-      it { expect(config.consumer_key).to eq('58132-f824d5fbf935681e22e86a3c') }
+      it { expect(config.authorization_token_file).to eq(File.expand_path('~/.pickpocket/authorization_token')) }
+      it { expect(config.oauth_token_file).to eq(File.expand_path('~/.pickpocket/oauth_token')) }
+      it { expect(config.article_list_file).to eq(File.expand_path('~/.pickpocket/article_list')) }
 
+      it { expect(config.consumer_key).to eq('58132-f824d5fbf935681e22e86a3c') }
       it { expect(config.pocket_homepage).to eq('https://getpocket.com') }
       it { expect(config.pocket_oauth_authorize_url).to eq('https://getpocket.com/v3/oauth/authorize') }
       it { expect(config.pocket_oauth_request_url).to eq('https://getpocket.com/v3/oauth/request') }

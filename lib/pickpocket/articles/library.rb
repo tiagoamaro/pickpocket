49763472c2ba07ea437d471c272f901de56458aa
Tiago Amaro <tiagopadrela@gmail.com>
2016-09-29 23:59:39 +0200

    introduce library concept with stub specs

--- lib/pickpocket/articles/library.rb
+++ lib/pickpocket/articles/library.rb
@@ -4,22 +4,58 @@ require 'yaml/store'
 module Pickpocket
   module Articles
     class Library
-      attr_reader :read, :unread
+      attr_reader :store
 
-      private def store
-        FileUtils.mkdir_p(Pickpocket.config.home_folder)
+      private def yaml_store
+        FileUtils.mkdir_p(File.dirname(Pickpocket.config.library_file))
         YAML::Store.new(Pickpocket.config.library_file)
       end
 
-      def initialize
-        @read   = []
-        @unread = []
+      private def guarantee_inventory
+        store.transaction do
+          store[:read]   = {} if store[:read].nil?
+          store[:unread] = {} if store[:unread].nil?
+        end
       end
 
-      # Library will have two arrays: read and unread
-      # Operations:
-      # => renew: replace the unread array with the given one
-      # => pick: select an unread article, put it to the read collection and return this article
+      def initialize
+        @api   = API.new
+        @store = yaml_store
+
+        guarantee_inventory
+      end
+
+      # Replace unread store with content from pocket
+      def renew
+        articles = @api.retrieve['list']
+        store.transaction do
+          store[:unread] = articles
+        end
+      end
+
+      # Select an unread article, put it to the read collection and return this article
+      def pick
+        store.transaction do
+          unread     = store[:unread]
+          random_key = unread.keys.sample
+
+          if (random_article = unread.delete(random_key))
+            store[:read].update({ random_key => random_article })
+
+            random_article
+          end
+        end
+      end
+
+      # Clear and return read articles hash
+      def clear_read
+        store.transaction do
+          read         = store[:read]
+          store[:read] = {}
+
+          read
+        end
+      end
     end
   end
 end
---
+++ spec/lib/articles/library_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Pickpocket::Articles
+  RSpec.describe Library do
+    pending 'TODO'
+  end
+end
